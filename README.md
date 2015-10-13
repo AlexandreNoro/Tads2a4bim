1b3dc7d2a1e710c565073c44271db725a18ba947
Alexandre Noro <Alexandre Noro@Acer_Noro>
2015-10-13 19:48:35 -0300

    Commit inicial, importação do projeto no GitHub.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Tads2a4bim
+# Tenologia em Análise e Desenvolvimento de Sistemas
+
+2º Ano, 4º Bimestre - 2015
+[Univel](http://univel.br)
+
+# 13 de outubro de 2015
+
+* Criação do repositório.
+* Criação do projeto.
+* Criação do Cliente e DAO.
---
+++ src/main/java/br/univel/cadastro/model/Cliente.java
@@ -0,0 +1,60 @@
+package br.univel.cadastro.model;
+
+public class Cliente {
+
+	private int id;
+	private String nome;
+	private String endereço;
+	private String telefone;
+	private String cidade;
+	private String uf;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereço() {
+		return endereço;
+	}
+
+	public void setEndereço(String endereço) {
+		this.endereço = endereço;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+}
