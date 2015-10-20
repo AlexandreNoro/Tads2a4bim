cde88eafab9e2e9c6affa59a450ec0557bf40255
Alexandre Noro <Alexandre Noro@Acer_Noro>
2015-10-20 20:52:01 -0200

    Criação de Relatório simples. Criação de Relatório simples com conexão ao banco. Criação de Relatório simples Ddireto para PDF.

---
+++ JasperSample/src/main/java/br/univel/SimpleReport.java
@@ -0,0 +1,59 @@
+package br.univel;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+
+import org.apache.commons.collections.map.HashedMap;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRTableModelDataSource;
+import net.sf.jasperreports.view.JasperViewer;
+
+public class SimpleReport {
+
+	private String arq = "C:\\Users\\Alexandre Noro\\JaspersoftWorkspace\\MyReports\\simples.jasper";
+
+	public SimpleReport() {
+
+		TableModel tableModel = TableModelData();
+
+		JasperPrint jasperPrint = null;
+		TableModelData();
+		try {
+			
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("empresa", "Petrobras");
+			map.put("telefone", "123pim567pim");
+			
+			jasperPrint = JasperFillManager.fillReport(arq, map,
+					new JRTableModelDataSource(tableModel));
+			JasperViewer jasperViewer = new JasperViewer(jasperPrint);
+			
+			//jasperViewer.setBounds(50, 50, 640, 480);
+			//jasperViewer.setLocationRelativeTo(null);
+	
+			jasperViewer.setVisible(true);
+		} catch (JRException ex) {
+			ex.printStackTrace();
+		}
+	}
+	//
+	private TableModel TableModelData() {
+		String[] columnNames = { "Id", "Nome", "Departamento", "Email" };
+
+		String[][] data = { { "1", "Hugo", "Financeiro", "hugod@univel.br" },
+				{ "2", "José", "Comercial", "josed@univel.br" },
+				{ "3", "Luiz", "Contábil", "luizd@univel.br" } };
+
+		return new DefaultTableModel(data, columnNames);
+	}
+
+	public static void main(String[] args) {
+		new SimpleReport();
+	}
+}
---
+++ JasperSample/src/main/java/br/univel/SimpleReportJdbc.java
@@ -0,0 +1,57 @@
+package br.univel;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.view.JasperViewer;
+
+public class SimpleReportJdbc {
+
+	private String arq = "C:\\Users\\Alexandre Noro\\JaspersoftWorkspace\\MyReports\\simplesjdbc.jasper";
+	private Connection con;
+
+	public SimpleReportJdbc() throws SQLException {
+
+		JasperPrint jasperPrint = null;
+		getConnection();
+		try {
+			jasperPrint = JasperFillManager.fillReport(arq, null,
+					getConnection());
+			JasperViewer jasperViewer = new JasperViewer(jasperPrint);
+			jasperViewer.setVisible(true);
+		} catch (JRException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	private Connection getConnection() throws SQLException {
+
+		String driver = "com.mysql.jdbc.Driver";
+
+		try {
+			Class.forName(driver);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Aguarde.....Abrindo Relatório!!!");
+		
+		String url = "jdbc:mysql://192.168.101.10/employees";
+		String user = "jasper";
+		String password = "jasper";
+		
+		return con = DriverManager.getConnection(url, user, password);
+		
+		
+	}
+
+	public static void main(String[] args) throws SQLException {
+		new SimpleReportJdbc();
+	}
+}
